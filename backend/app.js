6308bf89370149b9498acc8a745cdd166f853f59
Paulo Rocha <prsrocha88@gmail.com>
2019-03-16 18:02:20 +0000

    Add mongo and mongoose to project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
+// admin xKxlns1clyVWuzDU
 const app = express();
 
 app.use(bodyParser.json());
@@ -20,8 +22,11 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const posts = req.body;
-  console.log(posts);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  console.log(post);
   res.status(201).json({
     message: 'Post added successfully!'
   });
